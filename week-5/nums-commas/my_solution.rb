bfd407ec6a719c5b766203a90867bfd0079d1c68
cyanisis <sweetone06@gmail.com>
2016-03-11 01:01:22 -0700

    Final Solution with reflection

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -1,6 +1,6 @@
 # Numbers to Commas Solo Challenge
 
-# I spent [] hours on this challenge.
+# I spent [3] hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -26,56 +26,79 @@
 # 
 # 
 # 1. Initial Solution
-def separate_comma(i)
-	
-	input_length = i.to_s.length
-	output = []
-
-	if input_length <= 3
-		p i.to_s
-
-	elsif input_length > 3
-		# Take the input and split the integers into an array
-		input = i.to_s.split('')
-		# Iterate thru the array backwards
-		counter = 1
-		iteration = -1
-		while counter < input_length + 1
-		# Every third iteration, insert a comma
-			output.unshift(input[iteration])
-			if counter % 3 == 0 
-				output.unshift(",")
-			end
-			iteration -= 1
-			counter += 1
-		end	
-		# Return the array as a string
-		if input_length % 3 == 0
-			output.shift
-		end
-		p output.join
-	end
-end
-separate_comma(1121180)
+# def separate_comma(i)
+# 	
+# 	input_length = i.to_s.length
+# 	output = []
+# 
+# 	if input_length <= 3
+# 		p i.to_s
+# 
+# 	elsif input_length > 3
+# 		# Take the input and split the integers into an array
+# 		input = i.to_s.split('')
+# 		# Iterate thru the array backwards
+# 		counter = 1
+# 		iteration = -1
+# 		while counter < input_length + 1
+# 		# Every third iteration, insert a comma
+# 			output.unshift(input[iteration])
+# 			if counter % 3 == 0 
+# 				output.unshift(",")
+# 			end
+# 			iteration -= 1
+# 			counter += 1
+# 		end	
+# 		# Return the array as a string
+# 		if input_length % 3 == 0
+# 			output.shift
+# 		end
+# 		p output.join
+# 	end
+# end
+# separate_comma(1121180)
 
 
 # 2. Refactored Solution
-
+def separate_comma(i)
+	output = []
+	# Take the input and split the integers into an array
+	input = i.to_s.split('')
+	# Iterate thru the array backwards
+	counter = 1
+	iteration = -1
+	while counter < i.to_s.length + 1
+	# Every third iteration, insert a comma
+		output.unshift(input[iteration])
+		if counter % 3 == 0 
+			output.unshift(",")
+		end
+		iteration -= 1
+		counter += 1
+	end	
+	# Return the array as a string
+	if i.to_s.length % 3 == 0
+		output.shift
+	end
+	p output.join
+end
+separate_comma(180)
 
 
 
 # 3. Reflection
 
-#result_array = []
-#		counter = -1
-#		x = input_length
-#		while counter > input_length
-#			if counter % 3 == 0
-#			output += i.to_s[counter] + ","
-#		counter -= 1
-#			else
-#				output += i.to_s[counter]
-#				counter -= 1
-#			end
-#		end
-#		p output
+# Wow, I cannot believe how long I spent not coding trying to figure this out.  
+# easily 2 hours actively not coding just thinking about the problem.  I guess that 
+# is what I need practice doing so I'm glad i did it!  I definitely "leveled up" so to speak.
+# I had originally been on the right track but ultimately that original solution 
+# was going to end up being more trouble than it was worth.  I basically wanted to 
+# take care of the first four numbers in the integer, length wise.  then I was going to 
+# essentially put in the commas in everything else after that because the first 4 were 
+# handled differently... IE sometimes before the comma would have 1 2 or 3 integers.  
+# As I found out from working this problem, that was a fairly poor way to handle it, every
+# if I could find a way to brute force my way into getting solutions.  I had to 
+# 	do battle with the sunk cost fallacy and restart the logic a bit.  
+# 		I re thought how to solve the problem and it was a breeze.  I was forced to learn a lot of
+# 		cool ruby methods.  I learned how to manipulate an array much better than I had.
+# 		I looked up the .count method as well as iterated negatively.  It was great :)
