da55f2c869f83de81bd789e58caf14d2ed22a102
sweetkeys <shaun@shaunsweet.com>
2016-03-12 13:28:27 -0700

    Add reflection

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -7,8 +7,12 @@ def accountability_group
 	i = 1
 	input.each_slice(4) do |a| 
 		p "Accountability group number: #{i}"
+
 		p a
 		i += 1
 	end
 end
 accountability_group
+
+#Reflection
+# This assignment was extremely confusing to me.  The wording was poor and I go to it on the weekend so didn't have a way to clarify what DBC was wanting from me.  So I just put something together that did what they said.  Split a group of people into groups of 4.  I did have to look up a new method "each_slice" which was interesting to find!  
