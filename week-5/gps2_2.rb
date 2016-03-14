c3ae173bcd57d39e25dfb3fc7029f8c7b6a875fb
sweetkeys <shaun@shaunsweet.com>
2016-03-13 22:17:05 -0700

    Add gps solution

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -10,59 +10,82 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 def create_list(items)
-# steps: 
-  # take the string of words and create a hash with them as the key and the value default is 1
-  # hash the program works with
-  list = {} 	
+
+  hash = {} 	
   items.split.each do |x|
- 		list[x] = 1
+ 		hash[x] = 1
  	end
-  p list# print the list to the console [can you use one of your other methods here?]
+ 	hash
+ 	show_list(hash)
 end
-# output: hash with items(k) + qty(v)
-list = create_list("milk eggs bananas broccoli oranges cereal")
-
-
 
 # Method to add an item to a list
 # input: item name and optional quantity(default = 1)
- def add_item(item, list=list)
- # steps: add a new item to the hash
- list[item] = 1
- p "You have added a new item to the list, here is the new list: "
- end
- # output:confirmation of adding item to list
- add_item("chocolate", list)
- p list
-
+def add_item(item, hash=list)
+	hash[item] = 1
+	p "You have added a new item to the list, here is the new list: "
+	show_list(hash)
+end
 
 # Method to remove an item from the list
 # input:item
- def delete_item(item, list)
+def delete_item(item, hash)
  # steps: remove an item from the list
- list.delete(item)
+ hash.delete(item)
  p "Deleted item, the updated list is: " 
- p list
+ show_list(hash)
  # output:confirmation of item removal
- end
-
+end
 
 # Method to update the quantity of an item
 # input: item + new qty
-def update_quantity(item, quantity, list)
-# steps: read the item and find the item in the list
-list[item] = quantity
-# output: item and the new quantity as confirmation
-p list
+def update_quantity(item, quantity, hash)
+	# steps: read the item and find the item in the list
+	hash[item] = quantity
+	# output: item and the new quantity as confirmation
+	show_list(hash)
 end
-
-
 
 # Method to print a list and make it look pretty
 # input: string
-def show_list(list)
-# steps: 
-p list
-# output: the list formatted in a human readable format (not a raw hash readout lol)
+def show_list(hash)
+	# steps: 
+	hash.each {|k,v| p "#{k} | Qty: #{v}" }
+	# output: the list formatted in a human readable format (not a raw hash readout lol)
 end
+
+
+# interface with the program
+# puts "Welcome to my first grocery list program ^_^!.....\n\n"
+# puts "Lets first start by creating a list...  Simply enter in what you'd like to remember to buy with # everything separated by spaces.. ex:(milk oranges marshmallows eggs)\n\n"
+# #sleep(5)
+# puts "So now that you know how I work, lets get started!"
+# p "What items would you like to add?"
+# list = create_list(gets.chomp)
+# 
+# p "Great!  So we have made our list.  To show the list at any time, type 'show'."
+# help = p "The commands are thus: add, delete, update, show.  Type 'done' to finish and exit the program"
+# help
+# 
+# until gets.chomp == 'done'
+# 	puts "What are we doing next captain?"
+# 	
+# 	case gets.chomp
+# 	when "add"
+# 		p "What item do you want to add?"
+# 		add_item(gets.chomp, list)
+# 		
+# 	else
+# 		p "That isn't a valid command. "
+# 		help
+# 	end
+# end
+# 
+# 
+# 
+# 
+
+
+
+
 
