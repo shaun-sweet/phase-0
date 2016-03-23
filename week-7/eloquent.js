78d880aa621182e18be32439c5f92aa687f01b5f
cyanisis <sweetone06@gmail.com>
2016-03-22 20:19:40 -0700

    eloquent js added

---
+++ week-7/eloquent.js
@@ -0,0 +1,56 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+// prompt("what is your favorite food?");
+// console.log("hey, that is my favorite too");
+
+
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+// var x = "#";
+// for (i = 0; i < 7; i++) {
+// 	console.log(x);
+// 	x = x + "#";
+// }
+// Triangle
+
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+// function min(a, b) {
+// 	if (a > b) {
+// 		console.log(b);
+// 	}
+// 	else	{
+// 		console.log(a);
+// 	}
+// }
+// min(2,3)
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+// var me = {
+// 	name: "shaun",
+// 	age: 27,
+// 	favoriteFoods: ["pizza", "icecream", "beer"],
+// 	quirk: "I like pina colada"
+// };
+// console.log(me.favoriteFoods)
+
+// What are the biggest similarities and differences between JavaScript and Ruby?
+// I felt the gist was pretty identical.  Objects are definitely different in javascript.  They feel more like hashes than "objects".  There is a huge differences
+// in javascript syntax.  It is definitely not nearly as pretty.  Much it makes me feel more comfortable programming overall.
+
+// Was some of your Ruby knowledge solidified by learning another language? If so, which concepts??
+// I think that iteration was solidified a bit.  Also objects in general.  It was cool seeing how things like .length are just a property of the particular string you've invoked.
+// I related to that ruby
+
+// How do you feel about diving into JavaScript after reading these chapters?
+// I am excited! I am a little nervous to start, but excited to branch off a bit!
