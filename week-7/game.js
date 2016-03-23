13bc5256794cbac7e4a398dd66b3f4b7d1df8af9
cyanisis <sweetone06@gmail.com>
2016-03-22 22:00:16 -0700

    Add js variable info

---
+++ week-7/game.js
@@ -0,0 +1,49 @@
+ // Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission:
+// Goals:  I want to make a typing game mixed with some action RPG.  So basically, think sonic where you collect fairly meaningless coins.  You'll collect these coins and 
+//  once you hit a certain number of coins, you'll be able to turn into super sonic.  Once you can turn ito super sonic, you'll be able to kill the bad guy, which is 
+// baddie mcbadderface
+// You'll collect coins by simply having to type something simple, like a command get.coin or whatever.  That will get you a coin.  
+// Experiment with making it more difficult to acquire coins but MVP is this:
+// Simply start the game, as Goodington McGooder and you'll have your quest to gather coins, once you get the amount of coins needed, turn into Super Goodington McGooder
+// and relinquish the world of Baddie McBaddieface!  Are you up to the challenge?
+// Characters:
+// Goodington McGooder
+// Baddie McBaddieface
+// Objects:
+// Functions:
+
+// Pseudocode
+//
+//
+//
+//
+//
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
