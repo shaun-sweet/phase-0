4260ec09bb74f4fd33a41e6b1e8a8fea3c73408e
cyanisis <sweetone06@gmail.com>
2016-03-27 21:15:18 -0700

    add game and reflection

--- week-7/game.js
+++ week-7/game.js
@@ -23,13 +23,67 @@
 // 
 // 
 // Pseudocode
-// Game as a class.  The two characters are json objects within the class.  
-//
-//
+// declare a character class that holds baddie and goodie
+// define their health and their name as well as resources (baddie won't use this property)
+// Create resource method that increases on call
+// create an attack method that lowers the health of baddie by 1 + whatever resources you have.  The attack uses up your existing resources every attack and you start back at 0
 //
 //
 
 // Initial Code
+// 'use strict';
+// class Character{
+// 	constructor(name, health){
+// 		this.name = name;
+// 		this.resource = 0;
+// 		this.health = health;
+// 	}
+
+// 	checkHealth(){
+// 		if (this.health <= 0){
+// 			return "You win!!  VICTORY!"
+// 		}else{
+// 			return "keep going!! Maybe try getting a few resources!"
+// 		}
+// 	}
+
+// 	attack(){
+// 		if (this.name == "goodie") {
+// 			b.health = b.health - 1;
+// 			b.health = b.health - this.resource;
+// 			this.resource = 0;
+// 			console.log("health is " + b.health);
+// 			console.log("your health is " + g.health);
+// 			document.getElementById("b-health").innerHTML = "Health:" + b.health;
+// 			g.updateStats();
+// 			b.checkHealth();
+// 		}
+		
+// 	}
+
+// 	collectResource(){
+// 		// increase the resource count for goodie by 1 
+// 		this.resource++;
+// 		console.log("resources are now: " + this.resource);
+// 		document.getElementById("resources").innerHTML = "Resources: " + this.resource;
+// 	}
+
+// 	updateStats(){
+// 		document.getElementById("resources").innerHTML = "Resources: " + this.resource;
+// 		document.getElementById("b-health").innerHTML = "Health:" + b.health;
+// 		document.getElementById("score").innerHTML = b.checkHealth();
+// 	}
+// }
+
+// var b = new Character("baddie", 20);
+// var g = new Character("goodie", 20);
+// console.log(g);
+// console.log(b);
+
+
+
+
+// Refactored Code
 'use strict';
 class Character{
 	constructor(name, health){
@@ -38,15 +92,21 @@ class Character{
 		this.health = health;
 	}
 
+	checkHealth(){
+		if (this.health <= 0){
+			return "You win!!  VICTORY!"
+		}else{
+			return "keep going!! Maybe try getting a few resources!"
+		}
+	}
+
 	attack(){
 		if (this.name == "goodie") {
 			b.health = b.health - 1;
 			b.health = b.health - this.resource;
 			this.resource = 0;
-			console.log("health is " + b.health);
-			console.log("your health is " + g.health);
-			document.getElementById("b-health").innerHTML = "Health:" + b.health;
-			document.getElementById("result").innerHTML = this.resource;
+			g.updateStats();
+			b.checkHealth();
 		}
 		
 	}
@@ -54,28 +114,31 @@ class Character{
 	collectResource(){
 		// increase the resource count for goodie by 1 
 		this.resource++;
-		console.log("resources are now: " + this.resource);
-		document.getElementById("result").innerHTML = this.resource;
+		g.updateStats();
+	}
+
+	updateStats(){
+		document.getElementById("resources").innerHTML = "Resources: " + this.resource;
+		document.getElementById("b-health").innerHTML = "Health:" + b.health;
+		document.getElementById("score").innerHTML = b.checkHealth();
 	}
 }
-
 var b = new Character("baddie", 20);
 var g = new Character("goodie", 20);
-console.log(g);
-console.log(b);
-
-
-
-
-// Refactored Code
-
-
 
 
 
 
 // Reflection
-//
+//•	What was the most difficult part of this challenge?
+// Understanding if I was supposed to do it a “certain way”.  Starting was definitely the hardest part.
+// •	What did you learn about creating objects and functions that interact with one another?
+// I am still unsure I did it the way the assignment wanted.  But It seemed pretty easy!  I kept forgetting to call the object before trying to use the methods though :P
+// •	Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
+// I actually took the time to learn about the DOM functions and made the game interact with an HTML front end!  It was really fun!
+// •	How can you access and manipulate properties of objects?
+// Object.property = “value”
+
 //
 //
 //
