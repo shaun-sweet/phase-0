eceec03089fbab110e77807a85ad4c694ec6d6c9
cyanisis <sweetone06@gmail.com>
2016-03-29 00:32:52 -0700

    add review and fib

---
+++ week-7/ruby_review.rb
@@ -0,0 +1,30 @@
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+# Pseudocode
+
+
+
+# Initial Solution
+
+def is_fibonacci?(num)
+
+	if num <= 1
+		puts "yups"
+		p num = 2
+	else 
+	  p is_fibonacci?(num-1) + is_fibonacci?(num-2)
+	end
+end
+puts is_fibonacci?(5)
+
+
+# Refactored Solution
+
+
+
+
+
+
+
+# Reflection
